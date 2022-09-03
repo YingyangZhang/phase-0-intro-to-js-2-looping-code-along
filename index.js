120823f187541cb70850abf685a756a3041903d9
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-09-03 10:37:58 -0400

    commit tree 6efe4948de7796dedd5ee355c839c6b5549a9430

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+let message = [];
+
+function writeCards(myArray, myEvent){
+    for(let i = 0; i < myArray.length; i++){
+        message.push(`Thank you, ${myArray[i]}, for the wonderful ${myEvent} gift!`);
+    };
+    return message;
+}
+
+function countDown(myNum){
+    for(let i = myNum; i >= 0; i--){
+        console.log(i);
+    }
+}
